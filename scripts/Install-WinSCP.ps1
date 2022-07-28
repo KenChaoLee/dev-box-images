befc388056016b72f53b627f883911935bd81fb6
kgman1225 <kgman1225@gmail.com>
2022-07-28 15:08:54 +0800

    更新 winscp 安裝參數

--- scripts/Install-WinSCP.ps1
+++ scripts/Install-WinSCP.ps1
@@ -1,4 +1,4 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-choco install winscp -y --installargs="/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /o:PathOption=CmdTools /o:BashTerminalOption=ConHost /o:EnableSymlinks=Enabled /COMPONENTS=gitlfs" --no-progress
+choco install winscp -y --installargs="/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /o:PathOption=CmdTools /o:BashTerminalOption=ConHost /o:EnableSymlinks=Enabled " --no-progress
